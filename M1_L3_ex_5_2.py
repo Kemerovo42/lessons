07dca5460a14fdff9fecd98538bde2611e4094cf
Kemerovo42 <evgeniy_a_42@mail.ru>
2025-12-01 19:20:19 +0700

    Update M1_L3_ex_5_2.py

--- M1_L3_ex_5_2.py
+++ M1_L3_ex_5_2.py
@@ -1,8 +1,8 @@
 import random
 def drop_coin():
-  coin_a = random.randint(1,2)
-  if coin_a == 1:
-    coin = "Орёл"
-  else:
-    coin = "Решка"
-  print(coin)
+    coin_a = random.randint(1,2)
+    if coin_a == 1:
+        coin = "Орёл"
+    else:
+        coin = "Решка"
+    print(coin)
